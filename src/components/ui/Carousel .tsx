fb629af27747c66cc9d764598fe7547c1037fbdf
DIDAR-HOSSAIN <didarhossain018@gmail.com>
2023-11-02 13:46:41 +0600

    carousel responsive

--- src/components/ui/Carousel .tsx
+++ src/components/ui/Carousel .tsx
@@ -4,19 +4,20 @@ import slide from "@/assets/images/resource-images/slider-image/ladies.jpg";
 import slide1 from "@/assets/images/resource-images/slider-image/office.jpg";
 import slide2 from "@/assets/images/resource-images/slider-image/background.jpg";
 import Image from 'next/image';
+import "@/app/carousel.module.css"; // Import styles for small devices
 
 const CarouselSlide = () => {
   return (
     <div style={{ marginTop: "20px", borderRadius: "20px" }}>
       <Carousel autoplay>
         <div>
-          <ImageSlide image={slide} height={500} width={1400} />
+          <ImageSlide image={slide} />
         </div>
         <div>
-          <ImageSlide image={slide1} height={500} width={1400} />
+          <ImageSlide image={slide1} />
         </div>
         <div>
-          <ImageSlide image={slide2} height={500} width={1400} />
+          <ImageSlide image={slide2} />
         </div>
         {/* Add more div elements for additional images */}
       </Carousel>
@@ -24,22 +25,14 @@ const CarouselSlide = () => {
   );
 };
 
-const ImageSlide = ({ image, height, width }:any) => {
-  const imageStyle = {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    height: `${height}px`,
-  };
-
+const ImageSlide = ({ image }: any) => {
   return (
-    <div>
-      <div style={imageStyle}>
+    <div className="image-container">
+      <div className="image">
         <Image
-          width={width}
-          height={height}
           src={image}
           alt="Image"
+          layout="responsive"
         />
       </div>
     </div>
